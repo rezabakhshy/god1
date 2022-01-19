6babbc19d0988ae6cb57ec60baee376ba04143f6
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2022-01-19 12:33:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -310,16 +310,13 @@ def gif(client,message):
     response=requests.get(url)
     size=int(response.headers["content-length"])/1024/1024
     size=str(size)[:5]
-    x=len(url)
-    m=url[x-4:]
-    if m[0]!='.':
-        m=url[x-5:]
-    with open(f"@REZABZ2{m}","wb") as f:
+    file_name=os.path.basename(url)
+    with open(file_name,"wb") as f:
         f.write(response.content)
     message_id=message.message_id
     chat_id=message.chat.id
-    client.send_document(chat_id,f"@REZABZ2{m}",caption=f"\n**NAME:** @REZABZ2{m}\n**SAIZE:** {size} MB")
-    os.remove(f"@REZABZ2{m}")
+    client.send_document(chat_id,file_name,caption=f"\n**NAME:** {file_name}\n**SAIZE:** {size} MB")
+    os.remove(file_name)
 
 @app.on_message((filters.me) & filters.regex("^!help$"))
 def help(client,message):
