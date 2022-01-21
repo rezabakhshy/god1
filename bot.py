99868fbf3d1786e68442fae49755d9305ef07dd7
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2022-01-21 21:46:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,14 +28,7 @@ def f_to_gif(client,message):
     clip.write_gif("nowgif.gif")
     im = Image.open("nowgif.gif")
     frames = ImageSequence.Iterator(im)
-    size = 320, 240
-    frames = thumbnails(frames,size)
-    om = next(frames) # Handle first frame separately
-    om.info = im.info # Copy sequence info
-    om.save("nowgif.gif", save_all=True, append_images=list(frames))
-    im = Image.open("nowgif.gif")
-    frames = ImageSequence.Iterator(im)
-    size = 280, 200
+    size = 300, 220
     frames = thumbnails(frames,size)
     om = next(frames) # Handle first frame separately
     om.info = im.info # Copy sequence info
